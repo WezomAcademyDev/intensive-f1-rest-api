6a4b2e66c458542084a575c68c74acbf467d33a5
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2023-04-13 01:59:46 +0300

    check

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
+app.get('/v2/check', function (request, response) {
+	response.json({ message: 'Hello World!' });
+});
+
 app.post('/v2/subscribe', require('./api/subscribe-form'));
 
 app.listen(port, function () {
