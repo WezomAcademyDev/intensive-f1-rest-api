53fd9557212547ec46dd72ecb6ec3ddf59382b1d
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2023-04-13 03:05:39 +0300

    refactor

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,18 +1,20 @@
+const allowCors = require('../utils/allowCors');
+
 const getExistedSubscribers = () => {
 	return [{ email: 'exist@gmail.com' }, { email: 'sdf@sdf.sdf' }];
 };
 
 /**
- * @param {import('express').Request} request
- * @param {import('express').Response} response
+ * @param {import('@vercel/node').VercelRequest} request
+ * @param {import('@vercel/node').VercelResponse} response
+ * @return {import('@vercel/node').VercelResponse}
  */
-module.exports = function (request, response) {
+const handler = async (request, response) => {
 	const email = request.body.email;
 	if (!email) {
-		response.status(400).json({
+		return response.status(400).json({
 			message: `Пошта не була вказана! Вкажіть пошту`,
 		});
-		return;
 	}
 
 	const existedSubscribers = getExistedSubscribers();
@@ -21,13 +23,15 @@ module.exports = function (request, response) {
 	);
 
 	if (isExisted) {
-		response.status(400).json({
+		return response.status(400).json({
 			message: `Пошта "${email}" вже була додана до списку підписників раніше! Вкажіть іншу пошту`,
 		});
 	} else {
 		// save email to database ...
 		// send email to admin ...
 		// send message to user
-		response.json({ message: `Дякуємо за підписку!` });
+		return response.json({ message: `Дякуємо за підписку!` });
 	}
 };
+
+module.exports = allowCors(handler);
